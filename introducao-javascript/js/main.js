87c3c691a1e16270a1787bdb377dda7a939a01e7
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-05-19 20:22:48 -0300

    Manipulando dados - primeira <tr>|Validação com if

--- introducao-javascript/js/main.js
+++ introducao-javascript/js/main.js
@@ -1,5 +1,40 @@
 var titulo = document.querySelector('.titulo');
+titulo.textContent = 'Aparecida Nutricionista';
 
-//console.log(titulo);
+var tabelaPaciente = document.querySelector('#paciente1')
 
-titulo.textContent = 'Aparecida Nutricionista'; 
+var pesoDoPaciente = tabelaPaciente.querySelector('.info-peso').textContent;
+var alturaDoPaciente = tabelaPaciente.querySelector('.info-altura').textContent;
+var calculoImc = tabelaPaciente.querySelector('.info-imc');
+
+var imc = pesoDoPaciente / (alturaDoPaciente * alturaDoPaciente);
+
+var peso = true;
+var altura = true;
+
+if (pesoDoPaciente <= 0 || pesoDoPaciente >= 600) {
+    peso = false;
+    calculoImc.textContent = 'Peso Invalido!';
+}
+
+if (alturaDoPaciente <= 0 || alturaDoPaciente >= 3.00) {
+    altura = false;
+    calculoImc.textContent = 'Altura Invalida!';
+}
+
+if (peso && altura) {
+    calculoImc.textContent = imc;
+}
+
+
+
+/*
+//>>>>>> Uma meneira alternativa de fazer o exercicio. só tem que tomar cuidado com o operador '&&' pois
+// quando se usa ele tem que dar true nas duas validações. no meu caso, pesso e a altura, mas queremos evitar em qualquer
+// um dos "parametros"
+if ((pesoDoPaciente <= 0 || pesoDoPaciente >= 600) || (alturaDoPaciente <= 0 || alturaDoPaciente >= 3.00)) {
+    calculoImc.textContent = 'Peso ou Altura Invalido!';
+} else {
+    calculoImc.textContent = imc;
+}
+*/
