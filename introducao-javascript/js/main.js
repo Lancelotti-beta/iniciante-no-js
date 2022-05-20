ecf3e0ee6af70465c3e9be3486623fbd2218543c
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-05-20 11:02:32 -0300

    Teste de alterações de valores - laço 'for'

--- introducao-javascript/js/main.js
+++ introducao-javascript/js/main.js
@@ -1,30 +1,38 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista';
 
-var tabelaPaciente = document.querySelector('#paciente1')
+var tabelaPaciente = document.querySelectorAll('.paciente');
 
-var pesoDoPaciente = tabelaPaciente.querySelector('.info-peso').textContent;
-var alturaDoPaciente = tabelaPaciente.querySelector('.info-altura').textContent;
-var calculoImc = tabelaPaciente.querySelector('.info-imc');
+for (let index = 0; index < tabelaPaciente.length; index++) {
 
-var imc = pesoDoPaciente / (alturaDoPaciente * alturaDoPaciente);
+    var pesoDoPaciente = tabelaPaciente[index].querySelector('.info-peso').textContent;
+    var alturaDoPaciente = tabelaPaciente[index].querySelector('.info-altura').textContent;
+    var calculoImc = tabelaPaciente[index].querySelector('.info-imc');
+    
+    var imc = pesoDoPaciente / (alturaDoPaciente * alturaDoPaciente);
+    
+    var peso = true;
+    var altura = true;
+    
+    if (pesoDoPaciente <= 0 || pesoDoPaciente >= 600) {
+        peso = false;
+        calculoImc.textContent = 'Peso Invalido!';
+    }
+    
+    if (alturaDoPaciente <= 0 || alturaDoPaciente >= 3.00) {
+        altura = false;
+        calculoImc.textContent = 'Altura Invalida!';
+    }
+    
+    if (peso == false && altura == false) {
+        calculoImc.textContent = 'Valores Invalidos!'
 
-var peso = true;
-var altura = true;
-
-if (pesoDoPaciente <= 0 || pesoDoPaciente >= 600) {
-    peso = false;
-    calculoImc.textContent = 'Peso Invalido!';
+    } else if (peso && altura) {
+        calculoImc.textContent = imc.toFixed(2);
+        
+    }
 }
 
-if (alturaDoPaciente <= 0 || alturaDoPaciente >= 3.00) {
-    altura = false;
-    calculoImc.textContent = 'Altura Invalida!';
-}
-
-if (peso && altura) {
-    calculoImc.textContent = imc;
-}
 
 
 
